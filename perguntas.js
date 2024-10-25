b0d9f6164d911dba225c16215abf5d44424b3f97
Priscila Oliveira Alberton <145785226+Priscilaoliveiraalberton@users.noreply.github.com>
2024-10-25 08:39:39 -0300

    25/10

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Resposta',
+    'Geografia',
+    'Qual a montanha mais alta do brasil?',
+    'Pico da Neblina, o pico da Neblina é o ponto mais elevado do Brasil, contando com 2.995 metros',
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Resposta',
+    'Geografia',
+    'Em qual local da Ásia o português é língua oficial?',
+    'Macau, Macau tem duas lińguas duas línguas oficiais, Mandarim e português.Macau, uma região especial da china foi territoŕio até 1999',
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Resposta',
+    'Conhecimento Gerais',
+    'Qual maior animal terrestre?',
+    'Elefante Africano, o elefante africano é o maior terrestre. Ele pode medir até 4 metros de altura e 7 de comprimento e seu peso pode chegar até 8 toneladas',
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta?',
-    'Resposta',
+    'Geografia',
+    'Quanto tempo a luz do sol demora para chegar á terra?',
+    '8 minutos. Ela é calculada com base na distância do sol em relação á terra, que é de aproximadamente 1.500.000.000km, considerando que a velocidade da luz no vácuo é de 300.000 km/s',
 )
